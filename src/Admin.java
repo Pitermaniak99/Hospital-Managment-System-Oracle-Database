6c6c58b4a1e50ff148c32298db2703ade169f2ad
Pitermaniak99 <piotr.kozub1999@gmail.com>
2022-11-10 15:56:56 +0100

    Logout

--- src/Admin.java
+++ src/Admin.java
@@ -1,18 +1,32 @@
 import javax.swing.*;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 class Admin extends JFrame{
     JLabel title;
+    JButton loguot;
     Admin(){
         Font f=new Font("Arial",Font.BOLD,26);
         title=new JLabel("Panel administratora");
+        loguot=new JButton("Wyloguj");
         title.setFont(f);
         title.setBounds(50,30,350,50);
+        loguot.setBounds(1050,900,100,30);
         add(title);
+        add(loguot);
         setLayout(null);
         setVisible(true);
         setSize(1200,1000);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        loguot.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+                new Login();
+            }
+        });
     }
 
 
